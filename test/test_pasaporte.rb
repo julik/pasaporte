3cc8ab7069749db4a74407aaf42d4c8c75151930
julik <julik@65967661-7906-0410-b21e-e24373c15fab>
2007-09-29 23:32:46 +0000

    more Pasaporte tests
    git-svn-id: http://julik.textdriven.com/svn/tools/micro-apps/pasaporte/trunk@505 65967661-7906-0410-b21e-e24373c15fab

---
+++ test/test_pasaporte.rb
@@ -0,0 +1,101 @@
+require File.dirname(__FILE__) + '/helper'
+require 'flexmock'
+
+class TestProfilePage < Pasaporte::WebTest
+  fixtures :pasaporte_profiles
+  def setup
+    super
+    @request.domain = "id.company.net"
+  end
+
+  def test_user_info_page_for_user_who_never_logged_in
+    get '/unknown-stranger'
+    assert_response :success
+    assert_select "h3", true do | h |
+      assert_equal "<h3>This is <b>unknown-stranger's</b> page</h3>", h.to_s
+    end
+  end
+  
+  def test_user_info_page_for_a_user_who_is_in_hiding
+    get '/julik'
+    assert_response :success
+    assert_select "h3", true do | h |
+      assert_equal "<h3>This is <b>julik's</b> page</h3>", h.to_s
+    end
+  end
+  
+  def test_user_info_page_for_a_user_who_shows_his_profile
+    max = Profile.find(3)
+    assert max.shared?, "Max shares his profile"
+    get '/max'
+    assert_equal max.domain_name, @request.domain
+    assert_response :success
+    assert_not_nil @assigns.profile
+    
+    assert_select "h2", true do | h |
+      assert_equal "<h2>Max Lapshin</h2>", h.to_s
+    end
+    assert_match_body /Promised to/
+  end
+  
+  def test_user_info_depends_on_domain
+    get '/max'
+    assert_response :success
+    assert_not_nil @assigns.profile
+    
+    @request.domain = "trashcan.dot"
+    get '/max'
+    assert_response :success
+    assert_nil @assigns.profile, "A profile page for someone who never logged in does not show any details"
+  end
+    
+end
+
+class TestSignon < Pasaporte::WebTest
+  fixtures :pasaporte_profiles
+  def setup
+    super
+    @request.domain = "id.company.net"
+  end
+  
+  def teardown
+    returning(super) {Throttle.delete_all }
+  end
+  
+  def test_signon_displays_a_password_field
+    get '/julik/signon'
+    assert_response :success
+    assert_select "input[type=password]", true
+  end
+  
+  def test_posting_to_signon_should_call_auth_and_fail
+    flexmock(Pasaporte::AUTH).
+        should_receive(:call).with("julik", "trance", "id.company.net").
+        at_least.once.and_return(false)
+    
+    post '/julik/signon', :pass => "trance"
+    assert_response :success
+    assert_not_nil @assigns.msg, "Something should be put into msg"
+    assert_nil @state.nickname, "Nickname should not have been set in the session"
+    assert_not_nil @state.failed_logins, "The signon should start counting failed logins" 
+    assert_equal 1, @state.failed_logins, "The signon should have counted 1 failed login" 
+  end
+  
+  def test_posting_false_login_many_times_winds_the_failed_login_counter
+    flexmock(Pasaporte::AUTH).
+        should_receive(:call).with("julik", "trance", "id.company.net").
+        at_least.twice.and_return(false)
+    post '/julik/signon', :pass => "trance"
+    post '/julik/signon', :pass => "trance"
+    assert_equal 2, @state.failed_logins, "The failed login counter should have been wound" 
+  end
+
+  def test_past_the_failed_login_threshold_should_trottle
+    flexmock(Pasaporte::AUTH).
+        should_receive(:call).with("julik", "trance", "id.company.net").at_least.once.and_return(false)
+
+    post '/julik/signon', :pass => "trance"
+    post '/julik/signon', :pass => "trance"
+    post '/julik/signon', :pass => "trance"
+  end
+end
