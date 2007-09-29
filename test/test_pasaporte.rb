39cbc8c0ee19200a326f2ad4bfb70d4f9291962d
julik <julik@65967661-7906-0410-b21e-e24373c15fab>
2007-09-29 23:40:39 +0000

    What a day what a day
    git-svn-id: http://julik.textdriven.com/svn/tools/micro-apps/pasaporte/trunk@506 65967661-7906-0410-b21e-e24373c15fab

--- test/test_pasaporte.rb
+++ test/test_pasaporte.rb
@@ -89,13 +89,26 @@ class TestSignon < Pasaporte::WebTest
     post '/julik/signon', :pass => "trance"
     assert_equal 2, @state.failed_logins, "The failed login counter should have been wound" 
   end
-
+  
   def test_past_the_failed_login_threshold_should_trottle
     flexmock(Pasaporte::AUTH).
         should_receive(:call).with("julik", "trance", "id.company.net").at_least.once.and_return(false)
-
-    post '/julik/signon', :pass => "trance"
-    post '/julik/signon', :pass => "trance"
-    post '/julik/signon', :pass => "trance"
+    
+    Pasaporte::MAX_FAILED_LOGIN_ATTEMPTS.times { post '/julik/signon', :pass => "trance" }
+    assert_response :success
+    assert_match_body /I am stopping you/, "The throttling message should appear"
+    assert_equal 1, Throttle.count, "A throttle should have been made"
+  end
+  
+  def test_post_with_good_auth_shold_redirect_create_profile_and_munge_session
+    flexmock(Pasaporte::AUTH).
+        should_receive(:call).with("julik", "junkman", "id.company.net").once.and_return(true)
+    
+    post '/julik/signon', :pass => 'junkman'
+    
+    assert_response :redirect
+    assert_not_nil @assigns.profile, "The profile should have been hooked up"
+    assert_equal 2, @assigns.profile.id, "This is Julik's profile"
+    assert_equal 'julik', @state.nickname, "The nickname should be set to 'julik' so that the flag is present"
   end
 end
