dcef51cb206404ec1e3da40511abaa2ccf141a7a
julik <julik@65967661-7906-0410-b21e-e24373c15fab>
2008-11-06 09:14:20 +0000

    force :-(
    git-svn-id: http://julik.textdriven.com/svn/tools/micro-apps/pasaporte/trunk@825 65967661-7906-0410-b21e-e24373c15fab

--- lib/pasaporte/julik_state.rb
+++ lib/pasaporte/julik_state.rb
@@ -43,7 +43,7 @@ module JulikState
     initialize_session!
     @msg = @state.delete(:msg)
     returning(super(*a)) do
-      force_session_save! unless @state.empty?
+      force_session_save!
     end
   end
 end
